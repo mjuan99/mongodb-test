31919f8d4e323652a5ac8ca4b782ef0958c15133
mjuan99 <54592648+mjuan99@users.noreply.github.com>
2022-02-15 08:01:08 -0300

    fix: delete files from main

--- src/routes/healthRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const getHealthController = require('../controllers/getHealthController.js');
-
-
-router.get("/", getHealthController);
-
-
-module.exports = router;
