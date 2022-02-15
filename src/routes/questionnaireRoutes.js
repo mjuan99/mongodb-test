306674067e9033673afbaf77af41ed87961d15b2
mjuan99 <54592648+mjuan99@users.noreply.github.com>
2022-02-15 08:11:36 -0300

    feat: server implementation

---
+++ src/routes/questionnaireRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const postQuestionnaireController = require('../controllers/postQuestionnaireController.js');
+const getQuestionnaireController = require('../controllers/getQuestionnaireController.js');
+
+
+router.post("/", postQuestionnaireController);
+router.get("/:id", getQuestionnaireController);
+
+
+module.exports = router;
