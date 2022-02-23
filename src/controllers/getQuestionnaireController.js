c998066c65c37d5a753889d2f9e028bdfde7f8e4
mjuan99 <54592648+mjuan99@users.noreply.github.com>
2022-02-23 09:02:01 -0300

    fix: id type validation on get questionnaire

--- src/controllers/getQuestionnaireController.js
+++ src/controllers/getQuestionnaireController.js
@@ -3,13 +3,19 @@ const Questionnaire = require('../schemas/questionnaireSchema.js');
 
 async function getQuestionnaireController(req, res){
     try{
-        const questionnaire = await findQuestionnaire(Questionnaire, req.params.id);
-        if(questionnaire != null)
-            res.status(200).json({data: questionnaire});
-        else
-            res.status(404).send("Not Found");
+        const id = req.params.id;
+        let questionnaire = null;
+        if(mongoose.Types.ObjectId.isValid(id)){
+            questionnaire = await findQuestionnaire(Questionnaire, id);
+            if(questionnaire != null)
+                res.status(200).json({data: questionnaire});
+            else
+                res.status(404).send("Not Found");
+        }else
+            res.status(400).send("Bad Request");
     }
-    catch{
+    catch(err){
+        console.log(err);
         res.status(500).send("Server Error");
     }
 }
