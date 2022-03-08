c644338d91d260636c05eb0db62f140c0bf729f8
mjuan99 <54592648+mjuan99@users.noreply.github.com>
2022-03-08 10:25:14 -0300

    fix: logger encapsulation

--- src/index.js
+++ src/index.js
@@ -2,31 +2,9 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 
-const winston = require('winston');
-const {format} = winston;
-
-const printFormat = format.printf(({level, message, timestamp, requestID, ...metadata}) => {
-    const metadataString = JSON.stringify(metadata);
-    return `${timestamp} [${level}]${requestID ? ' request ' + requestID : ''}: ${message}${(metadataString != '{}') ? ' - ' + metadataString : ''}`;
-});
-const requestLogger = winston.loggers.add('request-logger', {
-    level: 'debug',
-    format: format.combine(
-        format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-        printFormat
-    ),
-    transports: [
-        new winston.transports.Console({level: 'info'}),
-        new winston.transports.File({filename: 'debug.log', level: 'debug'}),
-        new winston.transports.File({filename: 'info.log', level: 'info'}),
-        new winston.transports.File({filename: 'error.log', level: 'error'})
-    ],
-    exceptionHandlers: [
-        new winston.transports.Console(),
-        new winston.transports.File({filename: 'error.log'})
-    ]
-});
+const {requestLogger} = require('./logger/logger');
 requestLogger.debug('Debugging');
+
 
 const express = require('express');
 const requestID = require('express-request-id');
@@ -38,6 +16,7 @@ const healthRoutes = require('./routes/healthRoutes');
 const questionnaireRoutes = require('./routes/questionnaireRoutes');
 app.use('/health', healthRoutes);
 app.use('/questionnaire', questionnaireRoutes);
+
 
 app.listen(process.env.PORT, async () => {
     requestLogger.info('Server starting');
